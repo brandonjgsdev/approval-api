a4d89f65b17f955e5b053bf4213fd298ffe9ca22
brandonjgsdev <brandonjgs.dev@gmail.com>
2024-04-18 15:52:19 -0600

    feat: :sparkles: Laravel Passport and Fortify configured
    The auth and user admin has been configurated

---
+++ app/Providers/FortifyServiceProvider.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Providers;
+
+use App\Actions\Fortify\CreateNewUser;
+use App\Actions\Fortify\ResetUserPassword;
+use App\Actions\Fortify\UpdateUserPassword;
+use App\Actions\Fortify\UpdateUserProfileInformation;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Laravel\Fortify\Contracts\LoginResponse;
+use Laravel\Fortify\Fortify;
+use Illuminate\Http\Response;
+use Laravel\Fortify\Contracts\LogoutResponse;
+use Laravel\Fortify\Contracts\RegisterResponse;
+
+class FortifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->app->instance(LoginResponse::class, new class implements LoginResponse
+        {
+            public function toResponse($request)
+            {
+                $user = $request->user();
+                $token = $user->createToken('access_token');
+
+                $userArray = $user->toArray();
+                // $userArray['permissions'] = $user->getAllPermissions()->toArray();
+                // $userArray['role'] = "client1";
+
+                return response()->json([
+                    'status' => 'success',
+                    'message' => "¡Inicio de sesión exitoso!",
+                    "userData" => $user,
+                    'auth' => $token
+                ], Response::HTTP_OK);
+            }
+        });
+
+        $this->app->instance(RegisterResponse::class, new class implements RegisterResponse
+        {
+            public function toResponse($request)
+            {
+                return response()->json([
+                    'status' => 'success',
+                    'message' => "Tu registro se ha realizado de manera exitosa.",
+                ], Response::HTTP_CREATED);
+            }
+        });
+
+        $this->app->instance(LogoutResponse::class, new class implements LogoutResponse
+        {
+            public function toResponse($request)
+            {
+                return response()->json([
+                    'status' => 'success',
+                    'message' => 'Cerraste sesión con éxito.',
+                ], Response::HTTP_OK);
+            }
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Fortify::createUsersUsing(CreateNewUser::class);
+        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+        RateLimiter::for('login', function (Request $request) {
+            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())) . '|' . $request->ip());
+
+            return Limit::perMinute(5)->by($throttleKey);
+        });
+
+        RateLimiter::for('two-factor', function (Request $request) {
+            return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+    }
+}
